fe32c777dea3d348b3b15b426d330699ac1e60a4
forestmckinney <forestwmck@gmail.com>
2024-08-07 14:42:08 -0400

    extract private key from get token script

--- get_token.sh
+++ get_token.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-curl -X POST "https://accounts.spotify.com/api/token" -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=0f4344f0405a493e9060107a79b78903&client_secret=5d51a3bf55874bd7b9bde96bcbdceb8b" > key.json
+if [ $# -lt 1 ]; then
+    echo "client secret key must be provided as an argument"
+    exit 1
+fi
+
+curl -X POST "https://accounts.spotify.com/api/token" -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=0f4344f0405a493e9060107a79b78903&client_secret=${1}" > key.json
