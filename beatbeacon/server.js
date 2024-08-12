a0a56ff0d11fb51e4371b5c1dc9c3c53e7b5b45b
Jordunw <forestwmck@gmail.com>
2024-08-12 14:49:34 -0400

    rewrite index using react

--- beatbeacon/server.js
+++ beatbeacon/server.js
@@ -5,9 +5,13 @@ const app = express();
 
 const port = 3000;
 const hostname = "localhost";
+const redirectUrl = 'http://localhost:3000';
 
+const authorizationEndpoint = "https://accounts.spotify.com/authorize";
+const tokenEndpoint = "https://accounts.spotify.com/api/token";
+const scope = 'user-read-private user-read-email';
 
-const env = require("/Users/chrisfluta/Documents/GitHub/cs375/beatbeacon/env.json");
+const env = require("env.json");
 const Pool = pg.Pool;
 const pool = new Pool(env);
 pool.connect().then(function () {
@@ -16,9 +20,124 @@ pool.connect().then(function () {
 
 app.use(express.static("public"));
 
+const currentToken = {
+  get access_token() { return localStorage.getItem('access_token') || null; },
+  get refresh_token() { return localStorage.getItem('refresh_token') || null; },
+  get expires_in() { return localStorage.getItem('refresh_in') || null; },
+  get expires() { return localStorage.getItem('expires') || null; },
+
+  save: (response) => {
+    const {access_token, refresh_token, expires_in } = response;
+    localStorage.setItem('access_token', access_token);
+    localStorage.setItem('refresh_token', refresh_token);
+    localStorage.setItem('expires_in', expires_in);
+
+    const now = new Date();
+    const expiry = new Date(now.getTime() + (expires_in * 1000));
+    localStorage.setItem('expires', expiry);
+  }
+};
+
+// fetch auth code from current search URL on page load
+const oauth_search_args = new URLSearchParams(window.location.search);
+const oauth_code = args.get('code');
+
+if(oauth_code) {
+  const token = await getToken(code);
+  currentToken.save(token);
+
+  const url = new URL(window.location.href);
+  url.searchParams.delete('code');
+
+  const updatedUrl = url.search ? url.href : url.href.replace('?', '');
+  window.history.replaceState({}, document.title, updatedUrl);
+}
+
+if(currentToken.access_token) {
+  const userData = await getUserData();
+  // TODO: render logged in window here
+} else {
+  // TODO: render login window here
+}
+
+async function redirectToSpotifyAuthorize() {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const randomValues = crypto.getRandomValues(new Uint8Array(64));
+  const randomString = randomValues.reduce((acc, x) => acc + possible[x % possible.length], "");
+
+  const code_verifier = randomString;
+  const data = new TextEncoder().encode(code_verifier);
+  const hashed = await crypto.subtle.digest('SHA-256', data);
+
+  const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed))).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+
+  window.localStorage.setItem('code_verifier', code_verifier);
+
+  const authUrl = new URL(authorizationEndpoint);
+  const params = {
+    response_type: 'code',
+    client_id: env.client_id,
+    scope: scope,
+    code_challenge_method: 'S256',
+    code_challenge: code_challenge_base64,
+    redirect_uri: redirectUrl
+  };
+
+  authUrl.search = new URLSearchParams(params).toString();
+  window.location.href = authUrl.toString(); // redirect to auth server for login
+}
+
+async function getToken(code) {
+  const code_verifier = localStorage.getItem('code_verifier');
+
+  const response = await fetch(tokenEndpoint, {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      client_id: env.client_id,
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirectUrl,
+      code_verifier: code_verifier
+    })
+  });
+
+  return await response.json();
+}
+
+async function refreshToken() {
+  const response = await fetch(tokenEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      client_id: env.client_id,
+      grant_type: 'refresh_token',
+      refresh_token: currentToken.refresh_token
+    })
+  });
+
+  return await response.json();
+}
+
+async function getUserData() {
+  const response = await fetch("https://api.spotify.com/v1/me", {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + currentToken.access_token
+    }
+  });
+
+  return await response.json();
+}
+
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
-})
+});
 
 app.listen(port, hostname, () => {
   console.log(`Listening at: http://${hostname}:${port}`);
--- beatbeacon/src/components/home/indexContent.js
+++ beatbeacon/src/components/home/indexContent.js
@@ -1,9 +1,68 @@
+import React, { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import '../../styles/index.css';
+
 function MainPageContent() {
-    return (
-      <div class="title-headers-container title-img">
-        <h1 class="main-header">Title</h1>
+  // reference to map instance
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    // check if map already exists
+    if(mapRef.current) return;
+
+    // Creating map options
+    var mapOptions = {
+      center: [39.95659, -75.19548],
+      zoom: 19
+    }
+
+    // Creating a map object
+    mapRef.current = L.map('map', mapOptions);
+
+    // Creating a Layer object
+    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+    // Ask for current location and navigate to that area
+    mapRef.current.locate({ setView: true, maxZoom: 16 });
+
+    // Adding layer to the map
+    mapRef.current.addLayer(layer);
+
+    //messing around with markers
+    const customIcon = L.icon({
+      iconUrl: 'icon.png',
+      iconSize: [32, 32],
+      iconAnchor: [16, 32],
+      popupAnchor: [0, -32]
+    });
+
+    const marker = L.marker([39.95659, -75.19548], { icon: customIcon }).addTo(mapRef.current);
+    marker.bindPopup("CCI").openPopup();
+
+    mapRef.current.on('locationfound', function (e) {
+      var currentloc = L.marker(e.latlng, { icon: customIcon }).addTo(mapRef.current);
+      currentloc.bindPopup("current loc").openPopup();
+    });
+
+    // Cleanup map instance
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+        mapRef.current = null;
+      }
+    }
+  }, []);
+
+  return (
+    <>
+      <div class="header">BeatBeacon</div>
+      <div class="sidebar">
+        <a class="login-button">Login with Spotify</a>
       </div>
-    );
+      <div id="map"></div>
+    </>
+  );
 }
-  
+
 export default MainPageContent;
