f74877f59e16063a42a4d8937f89b3bdd3283477
forestmckinney <forestwmck@gmail.com>
2024-09-02 23:32:27 -0400

    spotify search display wip

--- beatbeacon/src/App.js
+++ beatbeacon/src/App.js
@@ -6,6 +6,7 @@ import Feed from "./components/home/Feed"; // Import the Feed component
 import Page404 from "./components/common/page404";
 import "./App.css";
 import LoginPage from "./components/home/login";
+import { SearchResults } from "./components/common/spotifyDisplay";
 
 class App extends React.Component {
   render() {
@@ -29,6 +30,10 @@ class App extends React.Component {
           {
             path: "/logout",
             element: <></>
+          },
+          {
+            path: "/test",
+            element: <SearchResults queryStr='gabrielle' width={300} height={200}/>
           }
         ],
       },
